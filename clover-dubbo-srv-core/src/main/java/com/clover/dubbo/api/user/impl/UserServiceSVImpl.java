211147c6f8f1f02df854319a947498a91994458a
UncleClover <qiang900714@126.com>
2019-07-08 20:40:01 +0800

    每次都是变更配置文件 哈哈

--- clover-dubbo-srv-core/src/main/java/com/clover/dubbo/api/user/impl/UserServiceSVImpl.java
+++ clover-dubbo-srv-core/src/main/java/com/clover/dubbo/api/user/impl/UserServiceSVImpl.java
@@ -1,7 +1,12 @@
 package com.clover.dubbo.api.user.impl;
 
 import com.clover.dubbo.api.user.interfaces.IUserServiceSV;
+import com.clover.dubbo.api.user.vo.UserVO;
 
 public class UserServiceSVImpl implements IUserServiceSV {
 
+	@Override
+	public UserVO queryUser() {
+		return null;
+	}
 }
